da6e6bc9324ad716ab75af91da0794a98262ebdb
Sukrita Janseetong <iam_sukrita@hotmail.com>
2020-07-04 13:43:59 +0700

    4. Complete

--- 62120501055-PEXAM-MDT112/62120501055-PEXAM-MDT112.ino
+++ 62120501055-PEXAM-MDT112/62120501055-PEXAM-MDT112.ino
@@ -6,9 +6,9 @@ Servo myservo;
 void setup(){
     Serial.begin(9600);
     myservo.attach(8);
-    myservo.write(0);
     myservo.write(180);
-    Serial.println("Servo : 180 degrees");
+    myservo.write(0);
+    Serial.println("Servo : 0 degrees");
      delay(900);
 }
 void loop(){
