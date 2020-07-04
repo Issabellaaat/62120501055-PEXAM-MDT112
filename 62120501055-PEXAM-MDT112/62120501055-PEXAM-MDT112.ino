95fa9a7993ad2a19f6e2093bc5f0595d209a2ec7
Sukrita Janseetong <iam_sukrita@hotmail.com>
2020-07-04 14:01:46 +0700

    6. Complete

--- 62120501055-PEXAM-MDT112/62120501055-PEXAM-MDT112.ino
+++ 62120501055-PEXAM-MDT112/62120501055-PEXAM-MDT112.ino
@@ -1,21 +1,7 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <SPI.h>
-#include <Wire.h>
-
-Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 32, &Wire);
-void setup(){
-    oled.begin(0x3C); // Address 0x3C for 128x32
-    oled.clearDisplay();
-    oled.setCursor(10, 10);
-
-  oled.setTextColor(SSD1306_WHITE);
-  oled.setTextSize(2);
-  oled.print("Hello MDT");
-  oled.display();
-
-  delay(1000);
-  oled.clearDisplay();
+void setup(){   
+    delay(650);
+    tone (8,600,400);
+  
 }
 void loop(){
 }
